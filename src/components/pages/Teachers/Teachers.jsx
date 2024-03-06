4885da4b7377041ceb748ae374fa00aa4c145c5e
Ivan Malakhovskyi <ivanmalahovskij121@gmail.com>
2024-03-06 21:50:08 +0200

    temp changes

--- src/components/pages/Teachers/Teachers.jsx
+++ src/components/pages/Teachers/Teachers.jsx
@@ -5,6 +5,7 @@ import { TeachersList } from "../../TeachersList/TeachersList";
 import { Test } from "../../Filters/Test";
 import { Container } from "../../layout/SharedLayout.styled";
 import { SectionFormContainer } from "../../layout/SharedLayout.styled";
+// import { FC } from "react";
 
 export const Teachers = () => {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App.tsx";
+import App from "./components/App.jsx";
 import "./fonts/Roboto/Roboto-Bold.ttf";
 import "./fonts/Roboto/Roboto-Medium.ttf";
 import "./fonts/Roboto/Roboto-Regular.ttf";
@@ -10,7 +10,10 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "./GlobalStyle.ts";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+//! Поставити ! після ('root')
+//! Перейменувати файл
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename="/learn-lingo-pet-project">
       <ThemeProvider theme={theme}>
