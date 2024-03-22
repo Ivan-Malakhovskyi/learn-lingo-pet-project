b149e87b35e60de0f350db38e7ff6df6953d8e23
Ivan Malakhovskyi <ivanmalahovskij121@gmail.com>
2024-03-22 17:49:15 +0200

    change setup vite and vervel env

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -4,25 +4,25 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const {
-  VITE_API_KEY,
-  VITE_AUTH_DOMAIN,
-  VITE_DATABASE_URL,
-  VITE_PROJECT_ID,
-  VITE_STORAGE_BUCKET,
-  VITE_MESSAGING_SENDER_ID,
-  VITE_APP_ID,
-  VITE_MEASUREMENT_ID,
+  VITE_VERCEL_API_KEY,
+  VITE_VERCEL_AUTH_DOMAIN,
+  VITE_VERCEL_DATABASE_URL,
+  VITE_VERCEL_PROJECT_ID,
+  VITE_VERCEL_STORAGE_BUCKET,
+  VITE_VERCEL_MESSAGING_SENDER_ID,
+  VITE_VERCEL_APP_ID,
+  VITE_VERCEL_MEASUREMENT_ID,
 } = import.meta.env;
 
 const firebaseConfig = {
-  apiKey: VITE_API_KEY,
-  authDomain: VITE_AUTH_DOMAIN,
-  databaseURL: VITE_DATABASE_URL,
-  projectId: VITE_PROJECT_ID,
-  storageBucket: VITE_STORAGE_BUCKET,
-  messagingSenderId: VITE_MESSAGING_SENDER_ID,
-  appId: VITE_APP_ID,
-  measurementId: VITE_MEASUREMENT_ID,
+  apiKey: VITE_VERCEL_API_KEY,
+  authDomain: VITE_VERCEL_AUTH_DOMAIN,
+  databaseURL: VITE_VERCEL_DATABASE_URL,
+  projectId: VITE_VERCEL_PROJECT_ID,
+  storageBucket: VITE_VERCEL_STORAGE_BUCKET,
+  messagingSenderId: VITE_VERCEL_MESSAGING_SENDER_ID,
+  appId: VITE_VERCEL_APP_ID,
+  measurementId: VITE_VERCEL_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-  // define process env
-  define: {
-    "process.env": process.env,
-  },
-  resolve: {
-    alias: {
-      src: "/src",
+  return {
+    plugins: [react(), svgr()],
+
+    // define process env
+
+    define: {
+      __APP_ENV__: JSON.stringify(env.__APP_ENV__),
     },
-  },
+    resolve: {
+      alias: {
+        src: "/src",
+      },
+    },
+  };
 });
