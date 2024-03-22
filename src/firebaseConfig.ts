3f12e5a7660387043af7476605bc6a62c2db8f08
Ivan Malakhovskyi <ivanmalahovskij121@gmail.com>
2024-03-22 17:16:01 +0200

    change setup firebase env key

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -4,25 +4,25 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const {
-  VITE_API_KEY,
-  VITE_AUTH_DOMAIN,
-  VITE_DATABASE_URL,
-  VITE_PROJECT_ID,
-  VITE_STORAGE_BUCKET,
-  VITE_MESSAGING_SENDER_ID,
-  VITE_APP_ID,
-  VITE_MEASUREMENT_ID,
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
 } = import.meta.env;
 
 const firebaseConfig = {
-  apiKey: VITE_API_KEY,
-  authDomain: VITE_AUTH_DOMAIN,
-  databaseURL: VITE_DATABASE_URL,
-  projectId: VITE_PROJECT_ID,
-  storageBucket: VITE_STORAGE_BUCKET,
-  messagingSenderId: VITE_MESSAGING_SENDER_ID,
-  appId: VITE_APP_ID,
-  measurementId: VITE_MEASUREMENT_ID,
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 // Initialize Firebase
